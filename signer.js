ae8748b92de6dc9b36673142bf6f5709560690aa
gbarcomu <gbarcomu@gmail.com>
2020-11-12 11:16:36 +0100

    Eslint

--- signer.js
+++ signer.js
@@ -1,23 +1,17 @@
 require('dotenv').config();
-const { ethers } = require("ethers");
-const { ABI } = require("./dummyABIandBytecode")
+const { ethers } = require('ethers');
+const { ABI } = require('./dummyABIandBytecode');
 
-const GAS_PRICE = "0x3B9ACA00";
-const GAS_LIMIT = "0x4630C0";
-const DESTINATION_ACCOUNT = "0x8ba1f109551bD432803012645Ac136ddd64DBA72";
-const ETHERS_SENT = "0.00001";
-
-module.exports = {
-  createRawTx,
-  createContractRawTx,
-  logBody,
-  getNonce,
-}
+const GAS_PRICE = '0x3B9ACA00';
+const GAS_LIMIT = '0x4630C0';
+const DESTINATION_ACCOUNT = '0x8ba1f109551bD432803012645Ac136ddd64DBA72';
+const ETHERS_SENT = '0.00001';
 
 class NonceManager {
   constructor() {
     this.nonce = 0;
   }
+
   getIncreaseNonce() {
     const currNonce = this.nonce;
     this.nonce += 1;
@@ -27,7 +21,7 @@ class NonceManager {
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.NODE_ENDPOINT);
 const wallet = new ethers.Wallet(process.env.PRIV_KEY, provider);
-let baseNonce = undefined;
+let baseNonce;
 const nonceManager = new NonceManager();
 const contract = new ethers.Contract(process.env.CONTRACT_ADDRESS, ABI, wallet);
 
@@ -36,28 +30,25 @@ async function getNonce() {
   return nonce;
 }
 
-
 async function getRawTransactionSigned(txNonce) {
-
   const tx = {
     to: DESTINATION_ACCOUNT,
     value: ethers.utils.parseEther(ETHERS_SENT),
     gasPrice: GAS_PRICE,
     gasLimit: GAS_LIMIT,
-    nonce: txNonce
-  }
+    nonce: txNonce,
+  };
 
   const rawTx = await wallet.signTransaction(tx);
   return rawTx;
 }
 
 async function getRawTransactionSignedFromContract(txNonce) {
-
   const tx = {
     gasPrice: GAS_PRICE,
     gasLimit: GAS_LIMIT,
-    nonce: txNonce
-  }
+    nonce: txNonce,
+  };
   const txContract = await contract.populateTransaction.add(); // Change this method for your method
   Object.assign(tx, txContract);
   const rawTx = await wallet.signTransaction(tx);
@@ -65,47 +56,53 @@ async function getRawTransactionSignedFromContract(txNonce) {
 }
 
 function createRawTx(requestParams, context, ee, next) {
-  if (baseNonce == undefined) {
-    getNonce().then(currNonce => {
+  if (baseNonce === undefined) {
+    getNonce().then((currNonce) => {
       baseNonce = currNonce;
-      getRawTransactionSigned(baseNonce + nonceManager.getIncreaseNonce()).then(rawTx => {
-        context.vars['rawTx'] = rawTx;
+      getRawTransactionSigned(baseNonce + nonceManager.getIncreaseNonce()).then((rawTx) => {
+        context.vars.rawTx = rawTx;
         return next();
       });
     });
-  }
-  else {
-    getRawTransactionSigned(baseNonce + nonceManager.getIncreaseNonce()).then(rawTx => {
-      context.vars['rawTx'] = rawTx;
+  } else {
+    getRawTransactionSigned(baseNonce + nonceManager.getIncreaseNonce()).then((rawTx) => {
+      context.vars.rawTx = rawTx;
       return next();
     });
   }
 }
 
 function createContractRawTx(requestParams, context, ee, next) {
-  if (baseNonce == undefined) {
-    getNonce().then(currNonce => {
+  if (baseNonce === undefined) {
+    getNonce().then((currNonce) => {
       baseNonce = currNonce;
-      getRawTransactionSignedFromContract(baseNonce + nonceManager.getIncreaseNonce()).then(rawTx => {
-        context.vars['rawTx'] = rawTx;
+      getRawTransactionSignedFromContract(baseNonce + nonceManager.getIncreaseNonce())
+        .then((rawTx) => {
+          context.vars.rawTx = rawTx;
+          return next();
+        });
+    });
+  } else {
+    getRawTransactionSignedFromContract(baseNonce + nonceManager.getIncreaseNonce())
+      .then((rawTx) => {
+        context.vars.rawTx = rawTx;
         return next();
       });
-    });
-  }
-  else {
-    getRawTransactionSignedFromContract(baseNonce + nonceManager.getIncreaseNonce()).then(rawTx => {
-      context.vars['rawTx'] = rawTx;
-      return next();
-    });
   }
 }
 
 function logBody(requestParams, response, context, ee, next) {
-  if(process.env.DEBUG === "true") {
+  if (process.env.DEBUG === 'true') {
     console.log(response.body);
-  }
-  else if(response.body.error) {
+  } else if (response.body.error) {
     console.log(response.body);
   }
   return next();
 }
+
+module.exports = {
+  createRawTx,
+  createContractRawTx,
+  logBody,
+  getNonce,
+};
